d0d9384a5a129c401c206b515b4906a3a6bea7ae
kekesam02 <1571828260@qq.com>
2019-03-11 18:28:06 +0800

    王五提交了Helloworld-1--解决冲突

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
 
     // 王五同学：222222222222222222222
-
     //张三添加11111111111111111
     public static void main(String[] args){
         System.out.println("Hello world!!");
